38d0b7d72ee089133d30d6b5d4e8aa5914c25c1e
Rendell Cale <rendellc@stud.ntnu.no>
2019-02-19 00:08:31 +0100

    Add dronelib examples

--- catkin_ws/src/techathon/scripts/example1.py
+++ catkin_ws/src/techathon/scripts/example1.py
@@ -8,31 +8,26 @@ import random
 import rospy
 from dronelib import Drone
 
-rospy.init_node("drone_test")
-drone = Drone()
+rospy.init_node("dronelib_example")
 
+drone = Drone()
 drone.activate()
 
 drone.takeoff()
-time.sleep(5)
 
-drone.set_target(5,0,3.14)
-time.sleep(5)
-
-drone.set_target(5,0,0)
-time.sleep(5)
-
-drone.set_target(3,0,0)
-time.sleep(5)
-
-drone.set_target(1,0,0)
+# Assume takeoff takes less than 5 seconds
 time.sleep(5)
 
 while not rospy.is_shutdown():
+    # generate some random point
     x = random.randint(-5, 5)
     y = random.randint(-5, 5)
     yaw = random.randint(0, 7)
+
+    # move to random point
     drone.set_target(x, y, yaw)
+
+    # sleep
     time.sleep(10)
 
 
