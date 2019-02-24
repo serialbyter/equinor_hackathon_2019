9e6f0342e049b702452aa5243c2dc657b1e3d622
ramunter <rasmusmunter@gmail.com>
2019-02-24 22:02:08 +0100

    Connect html to firebase, change scoreboard node to service

---
+++ catkin_ws/src/game_master/scripts/scoreboard_server.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python
+from __future__ import print_function
+
+import os
+import rospy
+import requests
+import pandas as pd
+
+from std_srvs.srv import Empty
+
+scorefile_path = os.path.join(os.path.dirname(__file__), '..')+"/scorefile1.txt"
+
+def add_new_scores_to_db(req):
+
+    #TODO team name server
+    print("here")
+    team = "usr1"
+
+    scores = pd.read_csv(scorefile_path, header=None,
+                                names=["Timestamp", "Score", "Sent"])
+
+    if not scores['Sent'].all():
+        update = scores[scores['Sent']==0]['Score'].values.tolist()
+        print(update)
+        result = requests.post(
+            'https://europe-west1-hackathon-af6d5.cloudfunctions.net/addScore',
+                data = {'team':team, 'score':update})
+    
+        if result.status_code == 200:
+            scores['Sent'].values[:] = 1
+            scores.to_csv(scorefile_path, header=False, index=False)
+
+def main():
+    #Init ROS node
+    rospy.init_node('scoreboard_server')
+    s = rospy.Service("publish_scores", Empty, add_new_scores_to_db)
+    while not rospy.is_shutdown():
+        rospy.spin();
+
+if __name__ == '__main__':
+    try:
+        main()
+    except rospy.ROSInterruptException:
+        pass
