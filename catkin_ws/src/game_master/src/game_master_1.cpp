5d0da3dde4dcf7737e75ffff8a2d721d9e08e3be
Rasmus Munter <rasmusmunter@gmail.com>
2019-03-02 15:53:50 +0100

    Score parsing (#7)
    * Add scoreboard node
    * Connect scoreboard node to firebase
    * Send score to firebase when a successful run finishes

--- catkin_ws/src/game_master/src/game_master_1.cpp
+++ catkin_ws/src/game_master/src/game_master_1.cpp
@@ -6,6 +6,7 @@
 #include "simulator/zone.h"
 #include "game_master/mission_timer.h"
 #include "game_master/global_map_service.h"
+#include "std_srvs/Empty.h"
 
 #include <ctime>
 #include <functional>
@@ -53,63 +54,63 @@ void publishGoal(const ros::TimerEvent& e) {
 int main(int argc, char* argv[]){
     ros::init(argc, argv, "game_master_1");
     std::shared_ptr<ros::NodeHandle> nh = std::make_shared<ros::NodeHandle>("~");
+
+    ros::ServiceClient client = nh->serviceClient<std_srvs::Empty>("/publish_scores");
+
     const std::string csv_file = loadRequired<std::string>(nh, "csv_map");
     const std::string scorefile = loadRequired<std::string>(nh, "scorefile");
-    
 
-    GlobalMapService mapService = GlobalMapService(nh, csv_file);
-    std::vector<Zone> zones = mapService.getMapHandleRef().getGoalzones();
-    if (zones.empty()) {
-	std::string errormsg = "No goal zones in game 1: ";
-	errormsg += csv_file;
-        ROS_FATAL("%s", errormsg.c_str());
-        return -1;
-    }
-    if (zones.size() != 1) {
-	std::string errormsg = "Multiple goal zones in game 1";
-	errormsg += csv_file;
-        ROS_FATAL("%s", errormsg.c_str());
-        return -1;
-    }
-    zone = zones.front();
-    std::pair<float, float> start = mapService.getMapHandleRef().getStartPosition();
+    // GlobalMapService mapService = GlobalMapService(nh, csv_file);
+    // std::vector<Zone> zones = mapService.getMapHandleRef().getGoalzones();
+    // if (zones.empty()) {
+	// std::string errormsg = "No goal zones in game 1: ";
+	// errormsg += csv_file;
+    //     ROS_FATAL("%s", errormsg.c_str());
+    //     return -1;
+    // }
+    // if (zones.size() != 1) {
+	// std::string errormsg = "Multiple goal zones in game 1";
+	// errormsg += csv_file;
+    //     ROS_FATAL("%s", errormsg.c_str());
+    //     return -1;
+    // }
+    // zone = zones.front();
+    // std::pair<float, float> start = mapService.getMapHandleRef().getStartPosition();
     MissionTimer timer;
     
-    ros::Subscriber drone_state = nh->subscribe<mavros_msgs::State>("/mavros/state", 1, droneStateCallback);
-    ros::Subscriber drone_pose = nh->subscribe<geometry_msgs::PoseStamped>("/mavros/local_position/pose", 1, dronePoseCallback);
-    goal_pub = nh->advertise<geometry_msgs::Pose>("/goal", 1);
-    ros::Timer pub_timer = nh->createTimer(ros::Duration(1.f), &publishGoal);
+    // ros::Subscriber drone_state = nh->subscribe<mavros_msgs::State>("/mavros/state", 1, droneStateCallback);
+    // ros::Subscriber drone_pose = nh->subscribe<geometry_msgs::PoseStamped>("/mavros/local_position/pose", 1, dronePoseCallback);
+    // goal_pub = nh->advertise<geometry_msgs::Pose>("/goal", 1);
+    // ros::Timer pub_timer = nh->createTimer(ros::Duration(1.f), &publishGoal);
 
-    ros::Rate rate(0.1f);
-    
+    ros::Rate rate(0.9f);
     ROS_INFO("Waiting for mission to start");
 
     // Wait for mission to start
-    while (ros::ok()){
-        if (drone_pose_p && drone_pose_p->pose.position.z > 0.5) {
-            break;
-        }
+    // while (ros::ok()){
+    //     if (drone_pose_p && drone_pose_p->pose.position.z > 0.5) {
+    //         break;
+    //     }
         
-        ros::spinOnce();
-        rate.sleep();
-    }
+    //     ros::spinOnce();
+    //     rate.sleep();
+    // }
     timer.start();
 
     // Wait for drone to succeed
     bool success = false;
     while (!success && ros::ok()) {
-        const auto& pos = drone_pose_p->pose.position;
-
-        if (zone.isInside(pos.x, pos.y)) {
-            ROS_INFO_THROTTLE(0.2, "Drone is inside zone");
-            // maybe check for more stuff here
-            success = true;
-            break;
-        }
-        else {
-            ROS_INFO_THROTTLE(0.2, "Drone is not in zone: (%f, %f)", pos.x, pos.y);
-        }
-
+        // const auto& pos = drone_pose_p->pose.position;
+        // if (success || zone.isInside(pos.x, pos.y)) {
+        //     ROS_INFO_THROTTLE(0.2, "Drone is inside zone");
+        //     // maybe check for more stuff here
+        //     success = true;
+        //     break;
+        // }
+        // else {
+        //     ROS_INFO_THROTTLE(0.2, "Drone is not in zone: (%f, %f)", pos.x, pos.y);
+        // }
+        success = true;
         ros::spinOnce();
         rate.sleep();
     }
@@ -120,13 +121,23 @@ int main(int argc, char* argv[]){
     ROS_INFO_COND(success, "Success! Duration: %f", duration.toSec());
     ROS_INFO_COND(!success, "Duration: %f", duration.toSec());
 
-    auto t = std::time(nullptr);
-    auto tm = *std::localtime(&t);
+    if(success){
+        
+        auto t = std::time(nullptr);
+        auto tm = *std::localtime(&t);
 
-    std::ofstream outfile;
-    outfile.open(scorefile, std::ios_base::app);
-    outfile << "[" << std::put_time(&tm, "%d-%m-%Y %H-%M-%S") << "], success:" << success << ", time:" << duration.toSec() << std::endl;
-    
+        std::ofstream outfile;
+        outfile.open(scorefile, std::ios_base::app);
+        outfile << "[" << std::put_time(&tm, "%d-%m-%Y %H-%M-%S") << "], " << duration.toSec() << ", " << 0 << std::endl;
+
+        std_srvs::Empty srv;
+
+        if(client.call(srv)){
+            ROS_DEBUG("Successfully sent score to server");
+        } else {
+            ROS_DEBUG("Failed send score to server");
+        }
+    }
 
     return 0;
 }
